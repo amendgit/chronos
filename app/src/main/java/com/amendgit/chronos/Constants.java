642973e372535ccd09e6043b30befe975b5c3428
erxiangbo <shijian0912@163.com>
2018-04-05 23:31:29 +0800

    remove unused empty line

--- app/src/main/java/com/amendgit/chronos/Constants.java
+++ app/src/main/java/com/amendgit/chronos/Constants.java
@@ -12,7 +12,6 @@ class Constants {
     static String ACTION_RESUME = "com.amendgit.chronos.Constants.ACTION_RESUME";
     static String ACTION_STOP = "com.amendgit.chronos.Constants.ACTION_STOP";
 
-
     static String NOTIFICATION_CHANNEL_ID_FOCUS = "com.amendgit.chronos.Constant.NOTIFICATION_CHANNEL_ID_FOCUS";
     static int NOTIFICATION_ID_FOCUS = 1;
 
