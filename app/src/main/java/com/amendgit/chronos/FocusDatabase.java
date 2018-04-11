a564f63d7ac47c18bf608b9ebc616390cafd2202
erxiangbo <shijian0912@163.com>
2018-04-11 15:31:25 +0800

    add Globals.java for global vars and FocusDatabase.java for database operation

---
+++ app/src/main/java/com/amendgit/chronos/FocusDatabase.java
@@ -0,0 +1,13 @@
+package com.amendgit.chronos;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by jash on 08/04/2018.
+ * github: https://github.com/amendgit
+ * email: shijian0912@163.com
+ * homepage: http:www.amendgit.com
+ */
+public class FocusDatabase {
+
+}
