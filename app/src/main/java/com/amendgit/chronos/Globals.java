d59b127dbad33fae84a876abad6a27ef715086b8
erxiangbo <shijian0912@163.com>
2018-04-12 14:21:29 +0800

    add open database logic to Globals

--- app/src/main/java/com/amendgit/chronos/Globals.java
+++ app/src/main/java/com/amendgit/chronos/Globals.java
@@ -1,6 +1,7 @@
 package com.amendgit.chronos;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 /**
  * Created by jash on 08/04/2018.
@@ -9,8 +10,20 @@ import android.database.sqlite.SQLiteDatabase;
  * homepage: http:www.amendgit.com
  */
 public class Globals {
+
+    final static String TAG = "Globals";
+
+    private static SQLiteDatabase db;
     public static SQLiteDatabase getDatabase() {
-        // toimpl
+        if (db != null) {
+            return db;
+        }
+        db = SQLiteDatabase.openDatabase("/sdcard/amendgit/matrix.db", null, 0);
+        if (db == null) {
+            Log.d(TAG, "open data base failed.");
+            return null;
+        }
+        db.execSQL("create table if not exists focus_events (id integer, startTime datetime, endTime datetime)");
         return null;
     }
 }
