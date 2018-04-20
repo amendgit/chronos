9790b3db9b229b36cc6e95b2d27e42a5f22215a7
erxiangbo <shijian0912@163.com>
2018-04-20 19:04:14 +0800

    migrate Checkmark ColorUtils etc from uhabits

---
+++ app/src/main/java/com/amendgit/chronos/Checkmark.java
@@ -0,0 +1,10 @@
+package com.amendgit.chronos;
+
+/**
+ * Created by jash on 16/04/2018.
+ * github: https://github.com/amendgit
+ * email: shijian0912@163.com
+ * homepage: http:www.amendgit.com
+ */
+public class Checkmark {
+}
