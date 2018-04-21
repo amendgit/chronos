349a0b3e24b2e77577bce8137d7296715f3bffc5
erxiangbo <shijian0912@163.com>
2018-04-21 09:19:08 +0800

    migrate ColorUtils and resolve errors.

--- app/src/main/java/com/amendgit/chronos/ColorUtils.java
+++ app/src/main/java/com/amendgit/chronos/ColorUtils.java
@@ -1,5 +1,9 @@
 package com.amendgit.chronos;
 
+import android.content.*;
+import android.graphics.*;
+import android.util.*;
+
 /**
  * Created by jash on 16/04/2018.
  * github: https://github.com/amendgit
@@ -7,4 +11,10 @@ package com.amendgit.chronos;
  * homepage: http:www.amendgit.com
  */
 public class ColorUtils {
+    public static int setMinValue(int color, float newValue) {
+        float hsv[] = new float[3];
+        Color.colorToHSV(color, hsv);
+        hsv[2] = Math.max(hsv[2], newValue);
+        return Color.HSVToColor(hsv);
+    }
 }
