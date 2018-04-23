6421da0f1699c11981c2937b4e59ef8ab5d78418
erxiangbo <shijian0912@163.com>
2018-04-23 10:54:14 +0800

    migrate InterfaceUtils.java and resolve errors.

--- app/src/main/java/com/amendgit/chronos/InterfaceUtils.java
+++ app/src/main/java/com/amendgit/chronos/InterfaceUtils.java
@@ -1,5 +1,10 @@
 package com.amendgit.chronos;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
 /**
  * Created by jash on 16/04/2018.
  * github: https://github.com/amendgit
@@ -7,4 +12,9 @@ package com.amendgit.chronos;
  * homepage: http:www.amendgit.com
  */
 public class InterfaceUtils {
+    public static float dpToPixels(Context context, float dp) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, metrics);
+    }
 }
